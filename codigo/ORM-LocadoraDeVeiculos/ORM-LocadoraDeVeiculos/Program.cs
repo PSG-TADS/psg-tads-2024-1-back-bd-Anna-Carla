eed2bba8cd4a9872c6ce1d6a3755575c21ff0ab9
Anna-Carla <annacarlateixeira16@gmail.com>
2024-04-14 14:44:52 -0300

    add: implementing classes and defining primary and foreign keys

---
+++ codigo/ORM-LocadoraDeVeiculos/ORM-LocadoraDeVeiculos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORM_LocadoraDeVeiculos
+{
+    internal class Program
+    {
+        public class Cliente
+        {
+            [Key]
+            public int Id { get; set; }
+            public string? Nome { get; set; }
+            public string? CPF { get; set; }
+            public string? Telefone { get; set; }
+            public string? Endereco { get; set; }
+            public ICollection<Reserva>? Reservas { get; set; }
+
+        }
+
+        public class Veiculo
+        {
+            [Key]
+            public int Id { get; set; }
+            public string? Placa { get; set; } 
+            public string? Modelo { get; set; }
+            public int Ano { get; set; }
+            public int? ReservaID { get; set; }
+            [ForeignKey("ReservaID")]
+            public string? Reserva { get; set; }
+
+        }
+
+        public class  Reserva
+        {
+            [Key]
+            public int Id { get; set; }
+            public string? DataRetirada { get; set; }
+            public string? DataDevolucao { get; set; }
+            public int? ClienteID { get; set; }
+            [ForeignKey("ClienteID")]
+            public string? Cliente { get; set;}
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
